0d8332a35e63d41ca8a6680501f2f9370f90e645
Charlotte Lui <charlotteytlui@gmail.com>
2020-03-25 20:30:26 +0200

    Updates

---
+++ precache-manifest.a97dda3ef6f6f4b68c7a1f207fc816b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b8db75620b80e8fb0d3a172e498228",
+    "url": "/advice-project/index.html"
+  },
+  {
+    "revision": "7277a2c670cba6c0a167",
+    "url": "/advice-project/static/css/main.dae7ba57.chunk.css"
+  },
+  {
+    "revision": "34a19ce9dd76bec6e437",
+    "url": "/advice-project/static/js/2.4b3513ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advice-project/static/js/2.4b3513ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7277a2c670cba6c0a167",
+    "url": "/advice-project/static/js/main.b7486388.chunk.js"
+  },
+  {
+    "revision": "a7c76d63bca01a2eec50",
+    "url": "/advice-project/static/js/runtime-main.677bc27b.js"
+  }
+]);
